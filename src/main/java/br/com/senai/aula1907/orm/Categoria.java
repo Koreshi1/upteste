c6e5b084207991ff090452900411ae703df5d698
Lucas Koerich <lucasmkoerich@gmail.com>
2023-08-26 16:11:03 -0300

    SA4

--- src/main/java/br/com/senai/aula1907/orm/Categoria.java
+++ src/main/java/br/com/senai/aula1907/orm/Categoria.java
@@ -14,6 +14,11 @@ public class Categoria {
 	private Integer id;
 	private String descricao;
 	private String ativo;
+	private Integer codigo_livro;
+	private String titulo_livro;
+	private String nome_autor;
+	private Integer isbn;
+	private String editora;
 	
 	public Integer getId() {
 		return id;
@@ -32,5 +37,35 @@ public class Categoria {
 	}
 	public void setAtivo(String ativo) {
 		this.ativo = ativo;
+	}
+	public Integer getCodigo_livro() {
+		return codigo_livro;
+	}
+	public void setCodigo_livro(Integer codigo_livro) {
+		this.codigo_livro = codigo_livro;
+	}
+	public String getTitulo_livro() {
+		return titulo_livro;
+	}
+	public void setTitulo_livro(String titulo_livro) {
+		this.titulo_livro = titulo_livro;
+	}
+	public String getNome_autor() {
+		return nome_autor;
+	}
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+	public Integer getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
 	}
 }
